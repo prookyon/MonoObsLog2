51b004eb8f687f534a8121328423f62c43ec7bd6
Rainer Ots <rainer@rainerots.eu>
2025-11-28 10:31:44 +0200

    Better error handling - base class

---
+++ include/ER.h
@@ -0,0 +1,34 @@
+//
+// Created by prook on 28.11.2025.
+//
+
+#ifndef MGWOBSLOG_RESULT_H
+#define MGWOBSLOG_RESULT_H
+
+#endif //MGWOBSLOG_RESULT_H
+
+#include <QString>
+
+enum class ErrorSeverity {
+    Warning,
+    Error,
+    Critical
+};
+
+struct ER {
+    ErrorSeverity severity;
+    QString errorMessage;
+
+    // Helper for failure
+    static ER Warning(const QString &msg) {
+        return {ErrorSeverity::Warning, msg};
+    }
+
+    static ER Error(const QString &msg) {
+        return {ErrorSeverity::Error, msg};
+    }
+
+    static ER Critical(const QString &msg) {
+        return {ErrorSeverity::Critical, msg};
+    }
+};
